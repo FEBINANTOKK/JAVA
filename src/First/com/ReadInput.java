852fd738dd12ccf77821f2b0b45b642ffe263900
unknown <febinanto61@gmail.com>
2024-11-29 17:21:13 +0530

    First commit

---
+++ src/First/com/ReadInput.java
@@ -0,0 +1,15 @@
+package First.com;
+import java.util.Scanner;
+
+class ReadInput{
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int Num=scn.nextInt();
+        char ch=scn.next().charAt(4);
+        String str=scn.next();
+        System.out.println(Num);
+        System.out.println(ch);
+        System.out.println(str);
+    }
+
+}
