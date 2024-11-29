5576f30596d789408a0464289722f3a2d335a0c7
unknown <febinanto61@gmail.com>
2024-11-29 18:25:40 +0530

    ADD Program using Switchcase to print values Size

---
+++ src/First/com/SizeOfEnteredValue.java
@@ -0,0 +1,26 @@
+package First.com;
+
+import java.util.Scanner;
+
+public class SizeOfEnteredValue {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int Value=scn.nextInt();
+        switch (Value){
+            case 29:
+                System.out.println("Small");
+                break;
+            case 30:
+                System.out.println("Medium");
+                break;
+            case 38:
+                System.out.println("Large");
+                break;
+            case 42:
+                System.out.println("XLarge");
+                break;
+            default:
+                System.out.println("Invalid Entry");
+        }
+    }
+}
