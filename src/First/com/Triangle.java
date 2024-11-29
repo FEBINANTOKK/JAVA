290a5a992eed295cc79120b60d6d86c27e272ac0
unknown <febinanto61@gmail.com>
2024-11-29 18:13:18 +0530

    ADD Program for Valid Triangle or not

---
+++ src/First/com/Triangle.java
@@ -0,0 +1,20 @@
+package First.com;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int Num1=scn.nextInt();
+        int Num2=scn.nextInt();
+        int Num3=scn.nextInt();
+        int angle=Num1+Num2+Num3;
+        if(angle==180){
+            System.out.println("Triangle can be formed\n" );
+        }else{
+            System.out.println("Triangle cannot be formed\n" );
+        }
+
+
+    }
+}
