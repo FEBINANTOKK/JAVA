f090f7d6eb772f93be155025b0a06cee65b23288
unknown <febinanto61@gmail.com>
2024-12-12 20:17:46 +0530

    solve some pattern problems

---
+++ src/Week3/com/CountDigitd.java
@@ -0,0 +1,17 @@
+package Week3.com;
+
+import java.util.Scanner;
+
+public class CountDigitd {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int n=scan.nextInt();
+        int count=0;
+        while(n>0){
+            n=n/10;
+            count++;
+
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/Week3/com/ReverseANumber.java
@@ -0,0 +1,21 @@
+package Week3.com;
+
+import java.util.Scanner;
+
+public class ReverseANumber {
+
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+        int n=scan.nextInt();
+int rev=0;
+        while(n>0){
+            int last=n%10;
+            rev=rev*10+last;
+            n=n/10;
+
+
+        }
+        System.out.println(rev);
+    }
+}
